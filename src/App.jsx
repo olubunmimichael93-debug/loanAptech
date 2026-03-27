5ac6fc5e1a8200146c74ed2230c288afb3fd2531
olubunmimichael93-debug <olubunmimichael93@gmail.com>
2026-03-27 10:54:03 +0100

    fix contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom' 
 import './App.css'
-import Contact from './components/contact'
+import Contact from './components/Contact'
 import Home from './pages/Home'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
